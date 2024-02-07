5f8adcf67be18db6819fef42222c8631bbc5cf2a
anilmhrzn <anilmaharjan995@gmail.com>
2024-02-07 22:22:51 +0545

    appbar and banner widget area done

---
+++ lib/main.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+var bannerItems = ["Burger", "Cheese Chilly", "Noodles", "Pizza"];
+var bannerImage = [
+  "images/burger.jpg",
+  "images/cheesechilly.jpg",
+  "images/noodles.jpg",
+  "images/pizza.jpg"
+];
+
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var screenHeight = MediaQuery.of(context).size.height;
+    var screenwidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: SizedBox(
+        height: screenHeight,
+        width: screenwidth,
+        child:  SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+                  // padding: const EdgeInsets.all(0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      IconButton(onPressed: null, icon: Icon(Icons.menu)),
+                      Text(
+                        "Foodies",
+                        style: TextStyle(
+                            fontSize: 25, fontFamily: "ProtestRevolution"),
+                      ),
+                      IconButton(onPressed: null, icon: Icon(Icons.person))
+                    ],
+                  ),
+                ),
+                BannerWidgetArea(),
+                 // Image.asset("cheesechilly.jpg"),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BannerWidgetArea extends StatelessWidget {
+  const BannerWidgetArea({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var screenHeight = MediaQuery.of(context).size.height;
+    var screenWidth = MediaQuery.of(context).size.width;
+    PageController controller = PageController(initialPage: 1);
+    List<Widget> banners = [];
+    for (int x = 0; x < bannerItems.length; x++) {
+      var bannerView = Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Container(
+          child: Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black38,
+                          offset: Offset(2.0, 2.0),
+                          blurRadius: 5.0,
+                          spreadRadius: 1.0)
+                    ]),
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                child: Image.asset(
+                  bannerImage[x],
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [Colors.transparent, Colors.black])),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      bannerItems[x],
+                      style: TextStyle(fontSize: 25.0, color: Colors.white),
+                    ),
+                    Text(
+                      "More than 40% Off",
+                      style: TextStyle(fontSize: 12.0, color: Colors.white),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+      banners.add(bannerView);
+    }
+
+    return Container(
+      width: screenWidth,
+      height: screenWidth * 9 / 16,
+      child: PageView(
+        controller: controller,
+        scrollDirection: Axis.horizontal,
+        children: banners,
+      ),
+    );
+  }
+}
+
