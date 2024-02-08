5c9d5ac69c91cbd20acae56cc6bab98648081615
anilmhrzn <anilmaharjan995@gmail.com>
2024-02-08 15:56:57 +0545

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const MyHomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
     );
   }
 }
@@ -27,60 +27,162 @@ var bannerImage = [
   "images/pizza.jpg"
 ];
 
-
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
   @override
   Widget build(BuildContext context) {
     var screenHeight = MediaQuery.of(context).size.height;
-    var screenwidth = MediaQuery.of(context).size.width;
+    var screenWidth = MediaQuery.of(context).size.width;
 
-    return Scaffold(
-      body: SizedBox(
-        height: screenHeight,
-        width: screenwidth,
-        child:  SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+    Future<List<Widget>> createList() async {
+      List<Widget> items = [];
+      String dataString =
+          await DefaultAssetBundle.of(context).loadString("assets/data.json");
+      List<dynamic> dataJSON = jsonDecode(dataString);
+
+      dataJSON.forEach((object) {
+        String finalString = "";
+        List<dynamic> dataList = object["placeItems"];
+        dataList.forEach((item) {
+          finalString = finalString + item + " | ";
+        });
+
+        items.add(Padding(
+          padding: EdgeInsets.all(2.0),
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black12,
+                      spreadRadius: 2.0,
+                      blurRadius: 5.0),
+                ]),
+            margin: EdgeInsets.all(5.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
-                  // padding: const EdgeInsets.all(0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      IconButton(onPressed: null, icon: Icon(Icons.menu)),
-                      Text(
-                        "Foodies",
-                        style: TextStyle(
-                            fontSize: 25, fontFamily: "ProtestRevolution"),
-                      ),
-                      IconButton(onPressed: null, icon: Icon(Icons.person))
-                    ],
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10.0),
+                      bottomLeft: Radius.circular(10.0)),
+                  child: Image.asset(
+                    object["placeImage"],
+                    width: 80,
+                    height: 80,
+                    fit: BoxFit.cover,
                   ),
                 ),
-                BannerWidgetArea(),
-                 // Image.asset("cheesechilly.jpg"),
+                SizedBox(
+                  width: 250,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(object["placeName"]),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 2.0, bottom: 2.0),
+                          child: Text(
+                            finalString,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontSize: 12.0,
+                              color: Colors.black54,
+                            ),
+                            maxLines: 1,
+                          ),
+                        ),
+                        Text(
+                          "Min. Order: ${object["minOrder"]}",
+                          style:
+                              TextStyle(fontSize: 12.0, color: Colors.black54),
+                        )
+                      ],
+                    ),
+                  ),
+                )
               ],
             ),
           ),
-        ),
+        ));
+      });
+
+      return items;
+    }
+
+    return Scaffold(
+      body: Container(
+        height: screenHeight,
+        width: screenWidth,
+        child: SafeArea(
+            child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+                    Text(
+                      "Foodies",
+                      style: TextStyle(fontSize: 50, fontFamily: "ProtestRevolution"),
+                    ),
+                    IconButton(icon: Icon(Icons.person), onPressed: () {})
+                  ],
+                ),
+              ),
+              BannerWidgetArea(),
+              SingleChildScrollView(
+                child: FutureBuilder(
+                    initialData: <List<Widget>>[],
+                    future: createList(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        List<Widget> widgets =
+                            snapshot.data!.whereType<Widget>().toList();
+                        return Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: ListView(
+                            primary: false,
+                            shrinkWrap: true,
+                            // scrollDirection: Axis.horizontal,
+                            children: widgets,
+                          ),
+                        );
+                      } else {
+                        return CircularProgressIndicator();
+                      }
+                    }),
+              )
+            ],
+          ),
+        )),
       ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.black,
+          child: Icon(
+            MdiIcons.food,
+            color: Colors.white,
+          )),
     );
   }
 }
 
 class BannerWidgetArea extends StatelessWidget {
-  const BannerWidgetArea({super.key});
-
   @override
   Widget build(BuildContext context) {
-    var screenHeight = MediaQuery.of(context).size.height;
     var screenWidth = MediaQuery.of(context).size.width;
-    PageController controller = PageController(initialPage: 1);
+
+    PageController controller =
+        PageController(viewportFraction: 0.8, initialPage: 1);
+
     List<Widget> banners = [];
+
     for (int x = 0; x < bannerItems.length; x++) {
       var bannerView = Padding(
         padding: EdgeInsets.all(10.0),
@@ -149,4 +251,3 @@ class BannerWidgetArea extends StatelessWidget {
     );
   }
 }
-
